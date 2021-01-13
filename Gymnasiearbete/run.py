530c6ff194dfff0c7d4ebd8c08a50ca51777683a
Mathias Wiström <mathiaswistrom02@gmail.com>
2021-01-13 16:07:42 +0100

    skapade funktionalitet för att starta servern

--- Gymnasiearbete/run.py
+++ Gymnasiearbete/run.py
@@ -1,1 +1,4 @@
-print('hej')
+from my_server import app        # importera "app" från huvudpaketet
+
+if __name__ == '__main__':       # startar servern
+    app.run(host='localhost', port=8080, debug=True)
