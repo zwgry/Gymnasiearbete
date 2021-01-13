1ac3c78549d21e094d4d79b32215b84b14c26287
Mathias Wiström <mathiaswistrom02@gmail.com>
2021-01-13 16:08:08 +0100

    skapade funktionalitet för routes

--- Gymnasiearbete/my_server/routes.py
+++ Gymnasiearbete/my_server/routes.py
@@ -1,1 +1,36 @@
-print('hej')
+from my_server import app
+from flask import render_template as rt
+from flask import request, redirect, url_for,flash, abort, session
+from my_server.databeasehandler import create_connection
+
+@app.route('/')
+@app.route('/index')
+@app.route('/home')
+@app.route('/start')
+def start():
+    return rt('index.html')
+
+
+@app.route('/categories')
+def categories():
+    pass
+
+@app.route('/category/<id>')
+def category(id=None):
+    if  id==None:
+        return redirect(url_for('categories'))
+    #TODO: skapa resten av denna route så att rätt kategori renderas
+    pass
+
+@app.route('/products')
+def products():
+    pass
+
+@app.route('/product/<id>')
+def product(id=None):
+    if id==None:
+        return redirect(url_for('products'))
+    #TODO: skapa resten av denna route så att rätt produkt renderas
+    pass
+
+
