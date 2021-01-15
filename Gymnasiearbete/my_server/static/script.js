dd7d86e29311dcf038e9df96ae0244b523c3b383
Lukas Strand <kryllan02@gmail.com>
2021-01-15 15:01:55 +0100

    ändrat design, börjat med sökfunktion

--- Gymnasiearbete/my_server/static/script.js
+++ Gymnasiearbete/my_server/static/script.js
@@ -1,1 +1,19 @@
-alert('hej');
+function search(inmatning) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let searchItem = JSON.parse(this.responseText);
+            if (searchItem == "") document.getElementById("list_employees").innerHTML = "";
+            else {
+                let s = "";
+                for (let i = 0; i < employees.length; i++) {
+                    s += "<tr><td>" + employees[i][1] + "</td><td>" + employees[i][3] + "</td></tr>";
+                }
+                document.getElementById("list_employees").innerHTML = s;
+            }
+        }
+    };
+    xhttp.open("POST", "/ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("inmatning=" + inmatning);
+}
