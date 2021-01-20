f99c2a323bbd824fea5b58f067334454de170972
Lukas Strand <kryllan02@gmail.com>
2021-01-20 16:06:18 +0100

    Sökfunktionen vidare arbete

--- Gymnasiearbete/my_server/static/script.js
+++ Gymnasiearbete/my_server/static/script.js
@@ -3,17 +3,17 @@ function search(inmatning) {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             let searchItem = JSON.parse(this.responseText);
-            if (searchItem == "") document.getElementById("list_employees").innerHTML = "";
+            if (searchItem == "") document.getElementById("x").innerHTML = "";
             else {
                 let s = "";
-                for (let i = 0; i < employees.length; i++) {
-                    s += "<tr><td>" + employees[i][1] + "</td><td>" + employees[i][3] + "</td></tr>";
+                for (let i = 0; i < searchItem.length; i++) {
+                    s += "<tr><td>" + searchItem[i][1] + "</td></tr>";
                 }
-                document.getElementById("list_employees").innerHTML = s;
+                document.getElementById("x").innerHTML = s;
             }
         }
     };
-    xhttp.open("POST", "/ajax", true);
+    xhttp.open("POST", "/search_products_categories", true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhttp.send("inmatning=" + inmatning);
 }
