8a22a8f77d8320cef61f8bb923776ed81fefb4bd
Lukas Strand <kryllan02@gmail.com>
2021-01-27 16:14:43 +0100

    Sökfunktionen är nu fullt fungerande

--- Gymnasiearbete/my_server/static/script.js
+++ Gymnasiearbete/my_server/static/script.js
@@ -8,21 +8,22 @@ function searchFunction(input) {
             dataType: "json",
             success: function(response) {
                 console.log(response);
-                    if (response == "") {
-                        $("#searchDropdownMenu").html("");
-                    } else {
-                        let s = "";
-                        s += "<h5 class='dropdown-header'>Produkt</h5>";
-                        for (let j = 0; j < response.length; j++ ) {
-                            for (let i = 0; i < response[j].length; i++) {
-                                s += "<a class='dropdown-item' href='#'>" + response[j][i][1] + "</a>";
-                            }
-                            if (j == 0) {
-                            s += "<h5 class='dropdown-header'>Kategori</h5>";
-                            }
-                        }
-                        $("#searchDropdownMenu").html(s);
+                if (response == "empty"){
+                    $("#searchDropdownMenu").hide();
+                    return null;
+                }  
+                let s = "";
+                s += "<h5 class='dropdown-header'>Produkt</h5>";
+                for (let j = 0; j < response.length; j++ ) {
+                    for (let i = 0; i < response[j].length; i++) {
+                        s += "<a class='dropdown-item' href='#'>" + response[j][i][1] + "</a>";
                     }
+                    if (j == 0) {
+                    s += "<h5 class='dropdown-header'>Kategori</h5>";
+                    }
+                }
+                $("#searchDropdownMenu").html(s);
+                $("#searchDropdownMenu").show();
             }
         });
     };
