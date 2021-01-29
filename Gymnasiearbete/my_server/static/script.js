9c38befba692d3e69cacc5e59b7829c33c6b97f9
Lukas Strand <kryllan02@gmail.com>
2021-01-29 14:55:48 +0100

    börjat med produktsida

--- Gymnasiearbete/my_server/static/script.js
+++ Gymnasiearbete/my_server/static/script.js
@@ -16,7 +16,7 @@ function searchFunction(input) {
                 s += "<h5 class='dropdown-header'>Produkt</h5>";
                 for (let j = 0; j < response.length; j++ ) {
                     for (let i = 0; i < response[j].length; i++) {
-                        s += "<a class='dropdown-item' href='#'>" + response[j][i][1] + "</a>";
+                        s += "<a class='dropdown-item search-text-overflow' href='#'>" + response[j][i][1] + "</a>";
                     }
                     if (j == 0) {
                     s += "<h5 class='dropdown-header'>Kategori</h5>";
