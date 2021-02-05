4a669e5b1120134dbb0f2c87fa955b0480f78a83
Lukas <kryllan02@gmail.com>
2021-02-05 14:14:52 +0100

    föreberedelser för merge

--- Gymnasiearbete/my_server/static/script.js
+++ Gymnasiearbete/my_server/static/script.js
@@ -28,26 +28,12 @@ function searchFunction(input) {
     });
 };
 
+var slideIndex = 1;
+
 $(document).ready(function(){
     
-    var startSlide = 1;
-    showSlides(startSlide);
+    showSlides(slideIndex);
 
-    function showSlides(n) {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        var dots = document.getElementsByClassName("dot");
-        if (n > slides.length) {slideIndex = 1}    
-        if (n < 1) {slideIndex = slides.length}
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";  
-        }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
-        }
-        slides[slideIndex-1].style.display = "block";  
-        dots[slideIndex-1].className += " active";
-    }
 });
 
 function plusSlides(n) {
@@ -56,4 +42,20 @@ function plusSlides(n) {
 
 function currentSlide(n) {
     showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i; 
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
 }
