9380f87b12b19a3b7e133e6e0499b1c3877fa0ad
Mathias Wiström <mathiaswistrom02@gmail.com>
2021-03-09 18:38:02 +0100

    Skickar nu med 6 produkter till startsidan

--- Gymnasiearbete/my_server/main/routes.py
+++ Gymnasiearbete/my_server/main/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask import render_template as rt 
 from my_server.models import Category, Product, Picture
+from random import randint
 
 main = Blueprint('main', __name__)
 
@@ -9,5 +10,13 @@ main = Blueprint('main', __name__)
 @main.route('/home')
 @main.route('/start')
 def start():
-    #Skicka bilder till index
-    return rt('index.html')
+    products_id = []
+    products = []
+    pictures = []
+    for i in range(6):
+        products_id.append(randint(1,Product.query.count()))
+
+    for id in products_id:
+        products.append(Product.query.filter_by(id=id).first())
+        pictures.append(Picture.query.filter_by(product_id=id).first())
+    return rt('index.html',products=products,pictures=pictures)
