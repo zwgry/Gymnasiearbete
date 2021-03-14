a9fce5a5fd9cb80b29832caf40303a731b3bddb8
Mathias Wiström <mathiaswistrom02@gmail.com>
2021-03-14 18:55:26 +0100

    gjorde klart funktionalliteten för errors

--- Gymnasiearbete/my_server/errors/routes.py
+++ Gymnasiearbete/my_server/errors/routes.py
@@ -3,6 +3,10 @@ from flask import render_template as rt
 
 errors = Blueprint('errors', __name__)
 
+@errors.app_errorhandler(401)
+def handle_404(err):
+    return rt('errors/401.html'), 401
+
 @errors.app_errorhandler(404)
 def handle_404(err):
     return rt('errors/404.html'), 404
