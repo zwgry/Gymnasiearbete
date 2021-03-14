269de15a1b086f7f485176b031f8d5c5ac441a13
Mathias Wiström <mathiaswistrom02@gmail.com>
2021-03-14 15:26:48 +0100

    fixade ett fel i cookies.js

--- Gymnasiearbete/my_server/static/cookies.js
+++ Gymnasiearbete/my_server/static/cookies.js
@@ -76,7 +76,7 @@ function shoppingCart(){
       let h="";
       var totalPrice = 0;
       for (let i = 0; i < response.length; i++) {
-        if (response[i]["stock"] <= 0) {
+        if (response[i]["stock"] > 0) {
           totalPrice += response[i]['price']*response[i]['stock']
         h+='<a href="/product/'+response[i]["id"]+'"><div class="shopping-cart-box color-lgray shopping-cart-color shopping-cart-link"><div class="shopping-cart-overflow shopping-cart-name"><p>'+response[i]["name"]+'</p></div><div class="shopping-cart-amount"><p>'+response[i]["stock"]+' st</p></div><div class="shopping-cart-price"><p>'+response[i]["price"]+' kr</p></div><div class="shopping-cart-trash" onclick=removeProduct('+response[i]["id"]+','+response[i]["stock"]+')><i class="fas fa-trash"></i></div></div></a>'
         }
