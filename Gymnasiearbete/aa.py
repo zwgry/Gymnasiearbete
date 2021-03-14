25a1a7806624f3bcb316c499aa42c5214f262a26
Mathias Wiström <mathiaswistrom02@gmail.com>
2021-03-14 20:28:24 +0100

    uppdaterade databasen med längre beskrivning till alla produkter

--- Gymnasiearbete/aa.py
+++
@@ -1,8 +0,0 @@
-from my_server import db
-from my_server.models import Product
-
-for product in Product.query.all():
-    product.description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel ullamcorper metus, id imperdiet urna. Sed semper lorem risus, sit amet tincidunt quam consequat quis. Mauris lectus nulla, euismod ac sem eget, volutpat lobortis massa. Integer imperdiet feugiat nisl eget iaculis. Sed commodo vitae felis ultrices facilisis. Mauris at malesuada eros, sit amet tincidunt arcu. Ut vehicula elit a imperdiet scelerisque. Sed sed odio sit amet felis porta scelerisque in eget turpis. Morbi venenatis nisl ac magna varius, aliquam bibendum mi cursus. Cras felis libero, mattis et fringilla non, blandit ac nisi. Sed ante justo, facilisis porttitor rutrum eu, pellentesque id mi. Vestibulum et mauris sit amet felis tristique mattis. '
-
-
-db.session.commit
